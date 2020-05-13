d34e2f32b7012ace44a876878ba9a9c5a4b7e68c
Rachel Bradley-Haas <rachel@mattermost.com>
2020-05-13 14:32:16 -0700

    updates

--- transform/snowflake-dbt/models/blp/enterprise_license_fact.sql
+++ transform/snowflake-dbt/models/blp/enterprise_license_fact.sql
@@ -10,7 +10,7 @@ WITH enterprise_license_fact AS (
         enterprise_license_mapping.licenseid,
         license_daily_details.start_date,
         enterprise_license_mapping.expiresat::date as expiresat,
-        license_daily_details.last_license_telemetry_date,
+        MAX(license_daily_details.last_license_telemetry_date) AS last_license_telemetry_date,
         MAX(COALESCE(license_daily_details.license_users,license_daily_details.customer_users))::int AS current_max_licensed_users,
         AVG(license_daily_details.license_server_dau)::int AS current_rolling_7day_avg_dau,
         AVG(license_daily_details.license_server_mau)::int AS current_rolling_7day_avg_mau,
@@ -23,8 +23,10 @@ WITH enterprise_license_fact AS (
         AVG(month_ago.license_server_mau)::int AS month_ago_rolling_7day_avg_mau,
         MAX(month_ago.license_server_dau)::int AS month_ago_max_license_server_dau,
         MAX(month_ago.license_server_mau)::int AS month_ago_max_license_server_mau,
-        ROUND(AVG(month_ago.servers),1) as month_ago_avg_servers,
-        MAX(month_ago.servers)::int as month_ago_max_servers
+        ROUND(AVG(month_ago.servers),1) AS month_ago_avg_servers,
+        MAX(month_ago.servers)::int AS month_ago_max_servers,
+        COUNT(distinct license_daily_details.servers) AS count_distinct_servers,
+        COUNT(*)
     FROM {{ ref('enterprise_license_mapping') }}
     LEFT JOIN {{ ref('license_daily_details') }} ON enterprise_license_mapping.licenseid = license_daily_details.license_id
         AND license_daily_details.date >= CURRENT_DATE - INTERVAL '8 days'
